eee997061ca0f194015a64a5577e18227c725733
Andrea Capolupo <lupoandrea98@hotmail.com>
2022-04-16 13:42:51 +0200

    Aggiunta VueApp
    VueApp non modificata + file di configurazione vue

---
+++ src/main/webapp/prolesson/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <img alt="Vue logo" src="../assets/logo.png" height="50">
+    <p>My Vue.js Application</p>
+      <nav>
+          <ul>
+              <li><router-link to="/">Home</router-link></li>
+              <li><router-link to="/about">About</router-link></li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader.vue'
+}
+</script>
+
+<style lang="scss">
+
+</style>
